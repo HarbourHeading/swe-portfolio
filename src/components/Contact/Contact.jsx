feb7890cf629c35286530c106a4e426fe080d852
HarbourHeading <Liam.a.ek05@gmail.com>
2024-06-17 17:36:03 +0200

    Changed `a` tag to be inside `li` instead of wrapping it for [proper screen reading](https://dequeuniversity.com/rules/axe/4.9/list)

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,30 +9,32 @@ export const Contact = () => {
                 <p>Feel free to reach out!</p>
             </div>
             <ul className={styles.links}>
-                <a href="mailto:liam.a.ek05@gmail.com">
-                    <li className={styles.link}>
+                <li className={styles.link}>
+                    <a href="mailto:liam.a.ek05@gmail.com">
                         <img src={getImageUrl("contact/email.png")} alt="Email icon"/>
                         <p>liam.a.ek05@gmail.com</p>
-                    </li>
-                </a>
-                <a target="_blank"
-                   rel="noopener noreferrer"
-                   aria-label="Link to my linkedin"
-                   href="https://www.linkedin.com/in/Liam-ek">
-                    <li className={styles.link}>
+                    </a>
+                </li>
+
+                <li className={styles.link}>
+                    <a target="_blank"
+                       rel="noopener noreferrer"
+                       aria-label="Link to my linkedin"
+                       href="https://www.linkedin.com/in/Liam-ek">
                         <img src={getImageUrl("contact/linkedin.png")} alt="LinkedIn icon"/>
                         <p>https://linkedin.com/in/liam-ek</p>
-                    </li>
-                </a>
-                <a target="_blank"
-                   rel="noopener noreferrer"
-                   aria-label="Link to my github"
-                   href="https://www.github.com/HarbourHeading">
-                    <li className={styles.link}>
+                    </a>
+                </li>
+
+                <li className={styles.link}>
+                    <a target="_blank"
+                       rel="noopener noreferrer"
+                       aria-label="Link to my github"
+                       href="https://www.github.com/HarbourHeading">
                         <img src={getImageUrl("contact/github.png")} alt="Github icon"/>
                         <p>https://github.com/HarbourHeading</p>
-                    </li>
-                </a>
+                    </a>
+                </li>
             </ul>
         </footer>
     )
