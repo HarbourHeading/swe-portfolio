c8252754ce545e67a191cc4654bc7f071370fbca
HarbourHeading <Liam.a.ek05@gmail.com>
2024-06-17 17:10:41 +0200

    Modified contact-me icons to be clickable links.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,27 +9,32 @@ export const Contact = () => {
                 <p>Feel free to reach out!</p>
             </div>
             <ul className={styles.links}>
-                <li className={styles.link}>
-                    <img src={getImageUrl("contact/email.png")} alt="Email icon"/>
-                    <a href="mailto:liam.a.ek05@gmail.com">liam.a.ek05@gmail.com</a>
-                </li>
-                <li className={styles.link}>
-                    <img src={getImageUrl("contact/linkedin.png")} alt="LinkedIn icon"/>
-                    <a target="_blank"
-                       rel="noopener noreferrer"
-                       aria-label="Link to my linkedin"
-                       href="https://www.linkedin.com/in/Liam-ek">https://linkedin.com/in/liam-ek
-                    </a>
-                </li>
-                <li className={styles.link}>
-                    <img src={getImageUrl("contact/github.png")} alt="Github icon"/>
-                    <a target="_blank"
-                       rel="noopener noreferrer"
-                       aria-label="Link to my github"
-                       href="https://www.github.com/HarbourHeading">https://github.com/HarbourHeading
-                    </a>
-                </li>
+                <a href="mailto:liam.a.ek05@gmail.com">
+                    <li className={styles.link}>
+                        <img src={getImageUrl("contact/email.png")} alt="Email icon"/>
+                        <p>liam.a.ek05@gmail.com</p>
+                    </li>
+                </a>
+                <a target="_blank"
+                   rel="noopener noreferrer"
+                   aria-label="Link to my linkedin"
+                   href="https://www.linkedin.com/in/Liam-ek">
+                    <li className={styles.link}>
+                        <img src={getImageUrl("contact/linkedin.png")} alt="LinkedIn icon"/>
+                        <p>https://linkedin.com/in/liam-ek</p>
+                    </li>
+                </a>
+                <a target="_blank"
+                   rel="noopener noreferrer"
+                   aria-label="Link to my github"
+                   href="https://www.github.com/HarbourHeading">
+                    <li className={styles.link}>
+                        <img src={getImageUrl("contact/github.png")} alt="Github icon"/>
+                        <p>https://github.com/HarbourHeading</p>
+                    </li>
+                </a>
             </ul>
         </footer>
-    );
+    )
+        ;
 };
