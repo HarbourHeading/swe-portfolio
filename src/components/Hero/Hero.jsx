c418f871f61576aabd0a4783abe20943c243c6cd
HarbourHeading <liam.e.swe@gmail.com>
2024-07-01 22:02:01 +0200

    Remove Java from hero summary.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,23 +6,22 @@ export const Hero = () => {
     return (
         <section className={styles.container}>
             <div className={styles.content}>
-                <h1 className={styles.title}>Hi, I&apos;m Liam.</h1>
+                <h1 className={styles.title}>Hi, I&apos;m Liam</h1>
                 <p className={styles.description}>
                     I am an ICT graduate and Software Engineer with five years of experience in the tech industry,
-                    primarily working with Java and
-                    Python.
+                    primarily working with Python.
                 </p>
                 <div className={styles.sub}>
                     <a href="mailto:liam.e.swe@gmail.com" className={styles.contactBtn}>
                         Contact Me
                     </a>
-                    <a aria-label="Link to my github" target="_blank" rel="noopener noreferrer" href="https://www.github.com/HarbourHeading">
+                    <a aria-label="Github link" target="_blank" rel="noopener noreferrer" href="https://www.github.com/HarbourHeading">
                         <img src={getImageUrl("contact/github.png")}
                              alt="github icon"
                              className={styles.subIcons}
                         />
                     </a>
-                    <a aria-label="Link to my linkedin" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/Liam-e-swe">
+                    <a aria-label="Linkedin link" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/Liam-e-swe">
                         <img src={getImageUrl("contact/linkedin.png")}
                              alt="linkedin icon"
                              className={styles.subIcons}
