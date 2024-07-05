4eac1b217001b77cfeb925e0db2f9de063598666
HarbourHeading <liam.e.swe@gmail.com>
2024-07-05 04:32:59 +0200

    Changed project layout

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -2,16 +2,25 @@ import styles from "./ProjectCard.module.css";
 import {getImageUrl} from "../../utils";
 import PropTypes from "prop-types";
 
-export const ProjectCard = ({ project: {title, imageSrc, description, skills, source}, }) => {
+export const ProjectCard = ({project: {title, imageSrc, description, skills, source},}) => {
 
 
     return (
-        <div className={styles.container}>
-            <img
-                src={getImageUrl(imageSrc)}
-                alt={title}
-                className={styles.image}
-            />
+        <div className={styles.card}>
+            <div>
+                <img
+                    src={getImageUrl("projects/file.png")}
+                    alt="File"
+                    className={styles.image_file}/>
+
+                <a href={source} aria-label="Github link" target="_blank" rel="noopener noreferrer">
+                    <img
+                        src={getImageUrl(imageSrc)}
+                        alt="Github"
+                        className={styles.image_github}
+                    />
+                </a>
+            </div>
             <h3 className={styles.title}>{title}</h3>
             <p className={styles.description}>{description}</p>
             <ul className={styles.skills}>
@@ -23,11 +32,6 @@ export const ProjectCard = ({ project: {title, imageSrc, description, skills, so
                     );
                 })}
             </ul>
-            <div className={styles.links}>
-                <a target="_blank" rel="noopener noreferrer" href={source} className={styles.link}>
-                    Source
-                </a>
-            </div>
         </div>
     );
 };
