e687c837a5270e2df64d82fd43ee147695c14b13
HarbourHeading <liam.e.swe@gmail.com>
2024-11-20 05:23:53 +0100

    FIX: Updated content security policy to be stricter. `.htaccess` file included for stricter HTTP requests, as well as equivalent policys in `vite.config.js` servers.

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,13 @@ export default defineConfig({
     strictPort: true,
   },
   server: {
+    headers: {
+        "Strict-Transport-Security": "max-age=86400; includeSubDomains",
+        "X-Content-Type-Options": "nosniff",
+        "X-Frame-Options": "DENY",
+        "X-XSS-Protection": "1; mode=block",
+        "Content-Security-Policy": "frame-ancestors 'self'",
+    },
     port: 5000,
     strictPort: true,
   },
