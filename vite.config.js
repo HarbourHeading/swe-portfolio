652d3670cb48eb1291ffa10f8cbde748e2a901da
Harbourheading <liam.e.swe@gmail.com>
2025-02-21 00:27:34 +0100

    BREAKING: Updated CSP policies. Replaced `index.html` meta tag for CSP with `.htaccess` HTTP headers and vite config policies.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import csp from "vite-plugin-csp-guard";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    csp({
+      dev: {
+        run: true,
+        outlierSupport: ["scss"]
+      },
+      policy: {
+        "script-src": ["'self'"],
+        "font-src": ["'self'", "data:"],
+        "img-src": ["'self'", "data:"],
+        'base-uri': ["'none'"],
+        "style-src": ["'self'"],
+        "object-src": ["'none'"],
+        "form-action": ["'none'"],
+      },
+      build: {
+        sri: true
+      }
+    }),
+  ],
   preview: {
     port: 5000,
     strictPort: true,
